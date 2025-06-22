e9df9274f6cbefb4543df64c891b837ca90e5717
jimhancs1 <jimhancsmuriithi@gmail.com>
2025-06-22 23:05:08 +0300

    Update exam_db.sql

--- exam_db.sql
+++ exam_db.sql
@@ -1,27 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 22, 2025 at 09:38 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Create the database
+CREATE DATABASE IF NOT EXISTS novel_db;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `exam_db`
---
-
--- --------------------------------------------------------
+-- Use the database
+USE novel_db;
 
 --
 -- Table structure for table `answers`
